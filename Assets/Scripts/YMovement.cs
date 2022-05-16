f539124765fb8fec928dbe2bf15935af4e32fda3
IIIEA <egor.solo2000@bk.ru>
2022-05-16 18:32:54 +0300

    Add gridbox textures

--- Assets/Scripts/YMovement.cs
+++ Assets/Scripts/YMovement.cs
@@ -1,7 +1,7 @@
 using System;
 using UnityEngine;
 
-public class YMovementHandler : MonoBehaviour
+public class YMovement : MonoBehaviour
 {
     [Header("Links")]
     [SerializeField] private SwipeMouseDetector _swipeMouseDetector;
@@ -9,6 +9,9 @@ public class YMovementHandler : MonoBehaviour
     [SerializeField] private float _currentVelocity;
     [SerializeField] private float _maxVelocity;
     [SerializeField] private float _minVelocity;
+
+
+    private Vector3 _velocity;
 
     private void OnEnable()
     {
@@ -22,6 +25,14 @@ public class YMovementHandler : MonoBehaviour
 
     private void OnSwipe(Vector2 delta)
     {
+        Vector3 pos = transform.position;
 
+        pos.y += _velocity.y * Time.deltaTime;
+
+        _velocity.y = Mathf.Lerp(_velocity.y, _maxVelocity, Time.deltaTime);
+
+        Debug.Log(_velocity.y);
+
+        transform.position = PositionSetter.SetPositionY(transform.position, pos.y);
     }
 }
