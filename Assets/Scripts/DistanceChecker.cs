0fb700fcd838249ea304f5a563b657da3b2d7e31
IIIEA <egor.solo2000@bk.ru>
2022-05-18 22:14:47 +0300

    Fixed rope bar

--- Assets/Scripts/DistanceChecker.cs
+++ Assets/Scripts/DistanceChecker.cs
@@ -8,44 +8,32 @@ public class DistanceChecker : MonoBehaviour
     [SerializeField] private float _maxDistance;
 
     private float _distancePassed = 0;
+    private float _points = 0;
     private float _currentDistance;
 
-    public float StartDistance { get; private set; }
-
-    public float MaxDistance 
-    {
-        get
-        {
-            return _maxDistance;
-        } 
-        private set
-        {
-            _maxDistance = value;
-        }
-    }
+    public float MaxDistance => _maxDistance;
 
     public event Action<float, float> DistanceChanged;
 
     private void Start()
     {
-        StartDistance = (transform.position - _point.position).magnitude;
-        MaxDistance = _maxDistance + StartDistance;
-        DistanceChanged?.Invoke(StartDistance, MaxDistance);
-        _currentDistance = StartDistance;
+        DistanceChanged?.Invoke(_distancePassed, MaxDistance);
+        _currentDistance = (transform.position - _point.position).magnitude;
     }
 
     private void Update()
     {
         if(_currentDistance != (transform.position - _point.position).magnitude)
         {
-            _currentDistance = (transform.position - _point.position).magnitude - _distancePassed;
+            _currentDistance = (transform.position - _point.position).magnitude;
+            var points = _currentDistance - _distancePassed;
+            _points += points;
+            _distancePassed += points;
 
-            if(_currentDistance < 0)
-            {
-                _currentDistance = 0;
-            }
+            Debug.Log(_points);
+            DistanceChanged?.Invoke(_points, MaxDistance);
 
-            DistanceChanged?.Invoke(_currentDistance, MaxDistance);
+
         }
     }
 
@@ -53,7 +41,7 @@ public class DistanceChecker : MonoBehaviour
     {
         if (other.GetComponent<RopeBuff>())
         {
-            _distancePassed += _plusDistnceSize;
+            _points -= _plusDistnceSize;
         }
     }
 }
