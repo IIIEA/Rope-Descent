6db4804154407c5d76f931883392a018af63b444
IIIEA <egor.solo2000@bk.ru>
2022-05-24 22:16:59 +0300

    Add damageable interface

---
+++ Assets/Scripts/PuppetController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuppetController : MonoBehaviour, IDamageable
+{
+    public void TakeDamage()
+    {
+        throw new System.NotImplementedException();
+    }
+}
